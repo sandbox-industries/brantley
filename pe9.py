52c514106388299b2a9fb15831acda2c3ed08e67
Corey Brantley <Brant742@gmail.com>
2020-04-14 12:20:08 -0600

    formatting

--- pe9.py
+++ pe9.py
@@ -1,11 +1,14 @@
 import time
+
 start = time.time()
 
 number = 1000
 
 for a in range(1, number + 1):
     for b in range(a + 1, number + 1 - a):
+
         c = number - a - b
+
         if ((a * a) + (b * b)) == (c * c):
             print(a * b * c)
 
