a1d03074b0da6f476e11a804999dfa75b6fb04a2
Corey Brantley <Brant742@gmail.com>
2020-05-21 10:56:05 -0600

    little clean up

--- pe42/pe42.py
+++ pe42/pe42.py
@@ -8,19 +8,10 @@ def get_nth_term(num):
     return (1/2) * num * (num + 1)
 
 
-# print(get_nth_term(10))
-
-
-# print((sqrt(111 - (sqrt(111) // 1)) % 1) == 0)
-
-
 def is_triangle_word(word):
     num = sum(list(map(lambda a: ord(a) - 64, word)))
 
-    if get_nth_term(sqrt(num * 2) // 1) == num:
-        return True
-    else:
-        return False
+    return get_nth_term(sqrt(num * 2) // 1) == num
 
 
 count = 0
@@ -28,12 +19,10 @@ for w in words:
     if is_triangle_word(w):
         count += 1
 print(count)
-#
-# print(sum([sqrt((sum(list(map(lambda a: ord(a) - 64, w))) * 2) - sqrt(sum(list(map(lambda a: ord(a) - 64, w))) * 2) // 1) % 1 == 0 for w in open('p042_words.txt', 'r').read().replace('"', '').split(',')]))
 
-# print(sqrt(90 - 9))
-# print()
-# print(sqrt(90 - 9) % 1)
+# One line using slightly different logic
+print(sum([sqrt((sum(list(map(lambda a: ord(a) - 64, w))) * 2) - sqrt(sum(list(map(lambda a: ord(a) - 64, w))) * 2) // 1) % 1 == 0 for w in open('p042_words.txt', 'r').read().replace('"', '').split(',')]))
+
 
 
 
